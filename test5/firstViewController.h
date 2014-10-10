e5598c66cfe02be37462f00d1dc5c5bb49634954
mads <mads@sina.com>
2014-10-10 14:46:42 +0800

    jia wen jian

---
+++ test5/firstViewController.h
@@ -0,0 +1,13 @@
+//
+//  firstViewController.h
+//  test5
+//
+//  Created by promo on 14-10-10.
+//  Copyright (c) 2014年 promo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface firstViewController : UINavigationController
+
+@end
---
+++ test5/firstViewController.m
@@ -0,0 +1,49 @@
+//
+//  firstViewController.m
+//  test5
+//
+//  Created by promo on 14-10-10.
+//  Copyright (c) 2014年 promo. All rights reserved.
+//
+
+#import "firstViewController.h"
+
+@interface firstViewController ()
+
+@end
+
+@implementation firstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
